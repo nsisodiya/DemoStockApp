d8bab80f02121e05e71f08c7c2c1e6af5677a77d
Narendra Sisodiya <narendra@narendrasisodiya.com>
2017-03-07 00:35:49 +0530

    Added Logic for color in Ticker Price.

--- src/components/TickerPrice.js
+++ src/components/TickerPrice.js
@@ -5,11 +5,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-export default function TickerPrice(prop) {
-  return <Title>{prop.newVal}</Title>;
+function decideBackgroundColor(props) {
+  let color;
+  if (props.oldVal === undefined) {
+    return 'white';
+  }
+  if (props.newVal < props.oldVal) {
+    color = 'red';
+  } else {
+    color = 'green';
+  }
+  return color;
 }
+function decideColor(props) {
+  let color;
+  if (props.oldVal === undefined) {
+    return 'black';
+  }
+  if (props.newVal < props.oldVal) {
+    color = 'blue';
+  } else {
+    color = 'white';
+  }
+  return color;
+}
+
+const Label = styled.span`
+  background: ${decideBackgroundColor};
+  color: ${decideColor};
+`;
+function TickerPrice(props) {
+  return <Label {...props}>{props.newVal}</Label>;
+}
+
+export default TickerPrice;
