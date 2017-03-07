a9802a79b9a6e9902c30af7aefefbeda9744d912
Narendra Sisodiya <narendra@narendrasisodiya.com>
2017-03-07 09:44:08 +0530

    Added timeAgo and working perfectly.

--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -4,8 +4,11 @@
 
 import React, { Component } from 'react';
 import mixin from '@nsisodiya/es6-mixins';
+import loopObject from 'loopobject';
+import Timeago from 'timeago.js';
 import StoreLoaderMixin from './StoreLoaderMixin';
 import TickerStore from '../logic/TickerStore';
+import TickerPrice from './TickerPrice';
 
 class StockTable extends Component {
   constructor() {
@@ -14,7 +17,37 @@ class StockTable extends Component {
   }
 
   render() {
-    return <pre>{JSON.stringify(this.state, null, "\t")}</pre>;
+    return <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Ticker</th>
+          <th>Price</th>
+          <th>Last Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          loopObject(this.state.stockData, (v, stockName) =>
+            <tr key={stockName}>
+              <td>
+                <div>{ stockName.toUpperCase() }</div>
+              </td>
+              <td>
+                {(() => {
+                  const oldProps = {};
+                  if (v.length >= 2) {
+                    oldProps.oldVal = v[v.length - 2].stockPrice;
+                  }
+                  return <TickerPrice {...oldProps} newVal={v[v.length - 1].stockPrice}/>;
+                })()}
+              </td>
+              <td>{new Timeago().format(v[v.length - 1].time)}</td>
+            </tr>, true)
+        }
+        </tbody>
+      </table>
+    </div>;
   }
 }
 mixin(StockTable, [StoreLoaderMixin]);
